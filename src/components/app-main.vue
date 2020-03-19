4b70cf04d4a647c0af18ca9db3350e1e3e34a9b7
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-19 22:04:58 +0100

    Integrate a video sample

--- src/components/app-main.vue
+++ src/components/app-main.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h1>Peer2School</h1>
+    <app-jitsi></app-jitsi>
   </div>
 </template>
 
 <style lang="scss"></style>
 
 <script>
+import AppJitsi from './app-jitsi'
 export default {
   name: 'app-main',
+  components: { AppJitsi },
   data() {
     return {
       counter: 0,
