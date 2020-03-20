75afffa3d2d0cb1cef3e9442ba434f45a7e2f2a6
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-20 10:28:30 +0100

    Do the video by our own

---
+++ src/components/app-webrtc.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss"></style>
+
+<script>
+export default {
+  name: 'app-webrtc',
+  data() {
+    return {}
+  },
+  methods: {},
+  async mounted() {
+  },
+}
+</script>
+
