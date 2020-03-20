3d3a5cad59e57e39fb962eff3cc45fe033d376f2
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-20 10:36:08 +0100

    Shows video

--- src/components/app-webrtc.vue
+++ src/components/app-webrtc.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
+  <video ref="video">
 
-  </div>
+  </video>
 </template>
 
 <style lang="scss"></style>
 
 <script>
+import Peer from 'simple-peer'
+
+const log = require('debug')('app:app-webrtc')
+
+navigator.getUserMedia = (navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia)
+
 export default {
   name: 'app-webrtc',
   data() {
@@ -14,6 +23,45 @@ export default {
   },
   methods: {},
   async mounted() {
+    const peer1 = new Peer({ initiator: true }) // you don't need streams here
+    const peer2 = new Peer()
+
+    // await this.$nextTick()
+
+    let video = this.$refs.video //  document.querySelector('video')
+    log('video', video)
+
+    peer1.on('signal', data => {
+      log('signal1', data)
+      peer2.signal(data)
+    })
+
+    peer2.on('signal', data => {
+      log('signal2', data)
+      peer1.signal(data)
+    })
+
+    peer2.on('stream', stream => {
+      log('stream')
+      // got remote video stream, now let's show it in a video tag
+
+      if ('srcObject' in video) {
+        video.srcObject = stream
+      } else {
+        video.src = window.URL.createObjectURL(stream) // for older browsers
+      }
+
+      video.play()
+    })
+
+    function addMedia(stream) {
+      log('addMedia')
+      peer1.addStream(stream) // <- add streams to peer dynamically
+    }
+
+    // then, anytime later...
+    navigator.getUserMedia({ video: true, audio: true }, addMedia, () => {
+    })
   },
 }
 </script>
