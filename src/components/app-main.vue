40e05d954dd7ff8824ff0ebf6645e0aefc4c8070
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-21 15:34:04 +0100

    A bit of layout

--- src/components/app-main.vue
+++ src/components/app-main.vue
@@ -1,8 +1,16 @@
 <template>
-  <app-peers></app-peers>
+  <div class="vstack">
+    <div class="header">Header</div>
+    <app-peers class="-fit"></app-peers>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.header {
+  background: #666;
+  height: 4rem;
+}
+</style>
 
 <script>
 import AppPeers from './app-peers'
