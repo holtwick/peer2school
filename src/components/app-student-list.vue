378298e2719fd1325817a370d95d1a3e956d9bdf
Flo Albrecht <florian.albrecht.public@outlook.de>
2020-03-21 19:27:55 +0100

    Stutend list add remove

--- src/components/app-student-list.vue
+++ src/components/app-student-list.vue
@@ -1,37 +1,44 @@
 <template>
   <div class="studentList" style="float: right">
     <h3>Student List</h3>
-        <div v-for="msg in state.chat">
-            {{msg.sender}}: <b>{{msg.msg}}</b>
-        </div>
+    <div v-for="student in this.pointingUp">{{ student }}</div>
   </div>
 </template>
 
 <style lang="scss"></style>
 
 <script>
-import { sendChatMessage } from '../state'
+import { sendChatMessage } from "../state";
 
 export default {
-  name: 'app-student-list',
+  name: "app-student-list",
   data() {
-    return {pointsOut : false}
+    return {
+      pointingUp: []
+    };
   },
   methods: {
     doSend() {
-      sendChatMessage(this.message)
-      this.message = ''
+      sendChatMessage(this.message);
+      this.message = "";
     },
-    pointOut() {
-    },
+    pointOut() {}
   },
-  watch : {
-    'state.chat' : function(newV, oldV) {
-      console.log("DEBUG DEBUG DEBUG: " + newV + "|" + oldV)
+  watch: {
+    "state.chat": function(val) {
+      const msg = val[val.length - 1];
+      console.log("DEBUG DEBUG DEBUG: " + msg);
+      if (msg.msg == "*Zeigt auf!*") this.pointingUp.push(msg.sender);
+      else if (msg.msg == "*Zeigt nicht mehr auf!*") {
+        for (var i = 0; i < this.pointingUp.length; i++) {
+          if (this.pointingUp[i] == msg.sender) {
+            this.pointingUp.splice(i, 1);
+          }
+        }
+      }
     }
   },
-  async mounted() {
-  },
-}
+  async mounted() {}
+};
 </script>
 
