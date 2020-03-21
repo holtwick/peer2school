4e3898b41c8f5598e5a6d4d464a02ac29d89a8b8
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-21 15:38:10 +0100

    Share URL

--- src/components/app-main.vue
+++ src/components/app-main.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="vstack">
-    <div class="header">Header</div>
+    <div class="header hstack">
+      <h1 class="-fit">peer.school</h1>
+      <div class="link">
+        Share <a :href="url">{{ url }}</a>
+      </div>
+    </div>
     <app-peers class="-fit"></app-peers>
   </div>
 </template>
 
 <style lang="scss">
 .header {
+  color: white;
   background: #666;
   height: 4rem;
+  align-items: center;
+
+  h1 {
+    margin-left: 1rem;
+    font-size: 2rem;
+  }
+
+  .link {
+    margin-right: 1rem;
+
+    a {
+      color: #e3e3e3;
+      text-decoration: none;
+    }
+  }
 }
 </style>
 
@@ -23,6 +44,11 @@ export default {
   },
   data() {
     return {}
+  },
+  computed: {
+    url() {
+      return location.href
+    },
   },
   methods: {},
   async mounted() {
