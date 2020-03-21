78aa6ae869a682a5177273b647c0499087e67210
Anselm Fehker <anselm@muenster.de>
2020-03-21 14:41:12 +0100

    try icon

--- src/components/app-chat.vue
+++ src/components/app-chat.vue
@@ -11,7 +11,7 @@
       </div>
     </form>
     <form @submit.prevent.stop="pointOut">
-      <button type ="submit">Melden</button>
+      <button type ="submit"><img src="img/aufzeigen.png"></button>
     </form>
   </div>
 </template>
