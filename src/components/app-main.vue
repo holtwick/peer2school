d2843f710ca23790b653f294c0bfbb5b9c8f800c
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-21 15:58:15 +0100

    Redirect URLs

--- src/components/app-main.vue
+++ src/components/app-main.vue
@@ -47,7 +47,7 @@ export default {
   },
   computed: {
     url() {
-      return location.href
+      return `https://peer.school/room/${this.state.room}`
     },
   },
   methods: {},
---
+++ web/class.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>peer.school</title>
+</head>
+<body>
+
+<?php
+
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL);
+
+  if (isset($_GET["room"])) {
+    $room = $_GET["room"];
+    $url = "https://holtwick.github.io/peer2school/dist/index.html#$room";
+  }
+
+?>
+
+<p>You will be redirected to <?php echo $url; ?></p>
+
+<script>
+location.assign('<?php echo $url; ?>')
+</script>
+
+</body>
+</html>
