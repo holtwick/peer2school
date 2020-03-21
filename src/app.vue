bc3ac311621cec7cd60045f2df6a23dfd670bf40
Flo Albrecht <florian.albrecht.public@outlook.de>
2020-03-21 17:29:01 +0100

    Added teacher GUI

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,21 @@
 <template>
-  <app-main id="app" class="app"/>
+  <span v-if="state.teacher">
+    <app-teacher id="app" class="app"/>
+  </span>
+  <span v-else>
+    <app-main id="app" class="app"/>
+  </span>
 </template>
 
 <script>
 
 import AppMain from './components/app-main'
+import AppTeacher from './components/app-teacher'
+
 
 export default {
   name: 'App',
-  components: { AppMain },
+  components: { AppMain, AppTeacher },
 }
 </script>
 
