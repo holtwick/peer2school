12a28cb45c481a803464ad293857c5fb24404f40
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-22 22:30:52 +0100

    Finger

--- src/components/app-signal.vue
+++ src/components/app-signal.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="signal" :class="{ '-active': active }">
-    <button @click="toggleSignal">
-      <img src="../assets/img/aufzeigen.png">
-    </button>
-  </div>
+  <button
+    @click="toggleSignal"
+    class="signal"
+    :class="{ '-active': active }"
+  >
+    <img src="../assets/img/aufzeigen.png">
+  </button>
 </template>
 
 <style lang="scss">
 .signal {
+  display: inline-block;
   margin-top: 1rem;
+  border-radius: 0.25rem;
+  background: #dedede;
+  padding: 0.5rem;
+
+  img {
+    display: inline-block;
+    background: transparent;
+  }
+
+  &:hover {
+    background: #ccc;
+  }
 
   &.-active {
-    background: orange;
+    background: rgba(255, 165, 0, 0.4);
   }
 }
 </style>
