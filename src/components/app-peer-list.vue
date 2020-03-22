a6412f2d35b818636863d20bc2da5fe6b8bc1786
amacado <clemens.bastian@gmail.com>
2020-03-22 14:03:51 +0100

    setup peer listing

---
+++ src/components/app-peer-list.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <b>Teilnehmer</b>
+        <ul>
+            <li v-for="peer in peers">
+                {{ getPeerNameById(peer.remote) }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import {getPeerNameBySenderId} from '../state';
+
+    export default {
+        name: "app-peer-list",
+        computed: {
+            peers: function() {
+                return this.state.status;
+            }
+        },
+        methods: {
+            getPeerNameById(id) {
+                return getPeerNameBySenderId(id);
+            },
+            async mounted() {
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
