7d069c0fbe300e1bb29e39a404bfc4af2f19cbb2
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-03-28 11:42:49 +0100

    Rework to get stuff into iframe

---
+++ src/components/app-jitsi.vue
@@ -0,0 +1,30 @@
+<template>
+  <iframe
+    src="/jitsi.html"
+    frameborder="0"
+    marginheight="0"
+    marginwidth="0"
+    scrolling="no"
+    class="iframe"
+    style="overflow: hidden;"
+    ref="iframe"></iframe>
+</template>
+
+<style lang="scss">
+.iframe {
+  width: 100%;
+}
+</style>
+
+<script>
+export default {
+  name: 'app-jitsi',
+  data() {
+    return {}
+  },
+  methods: {},
+  async mounted() {
+  },
+}
+</script>
+
