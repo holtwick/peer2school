b09a4707fb88816fbf0f6ace4671c4385a439271
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-04-22 17:04:17 +0200

    Own STUN and TURN to avoid Google and Twilio. Fixes #2

--- src/config.js
+++ src/config.js
@@ -3,3 +3,16 @@ export const ENABLE_MEDIASERVER = false
 
 export const LOCAL_ID = 'id'
 export const LOCAL_NAME = 'name'
+
+// See https://github.com/feross/simple-peer#peer--new-peeropts
+export const ICE_CONFIG = {
+  iceTransportPolicy: 'all',
+  reconnectTimer: 3000,
+  iceServers: [{
+    urls: 'stun:turn01.brie.fi:5349',
+  }, {
+    urls: 'turn:turn01.brie.fi:5349',
+    username: 'brie',
+    credential: 'fi',
+  }],
+}
