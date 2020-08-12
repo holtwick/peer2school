e4d1584bfe3be7663b6033446f27831905d807d1
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-08-12 15:50:41 +0200

    Fix fr

--- src/jitsi/main.js
+++ src/jitsi/main.js
@@ -3,6 +3,7 @@ import locale from '../lib/locale'
 import de from '../locales/de'
 import en from '../locales/en'
 import it from '../locales/it'
+import fr from '../locales/fr'
 import App from './jitsi-main.vue'
 import { state } from './state'
 
@@ -15,7 +16,7 @@ Vue.mixin({
 })
 
 Vue.use(locale, {
-  locales: { en, de, it },
+  locales: { en, de, it, fr },
 })
 
 new Vue({
